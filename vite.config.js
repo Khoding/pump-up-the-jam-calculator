74d402eef28b98a9404dccc4da0881b60d12a0dc
Julien Rutscho <58421711+Khoding@users.noreply.github.com>
2025-03-19 23:31:03 +0100

    PWA

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,39 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pump Up The Jam Calculator',
+        short_name: 'PUTJ Calculator',
+        description:
+          'Time difference between a date and the release of unrelated Techno anthem "Pump Up The Jam"',
+        theme_color: '#a599e9',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerSW: {
+        immediate: false,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

694fab2bbf3a6fa40223f82ad5f10a25f789702d
Julien Rutscho <khodokpro@gmail.com>
2025-03-25 12:48:34 +0100

    Better handling of the text and remove unrelated

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,20 +3,22 @@
     <DatePicker v-model="selectedDate" />
 
     <p class="countdown-units" v-if="isToday(userDate)">Today is</p>
-    <nav v-else class="center-align">
+
+    <nav v-else class="edit-prefix center-align no-margin">
       <p class="countdown-units" v-if="!isEditing">{{ modelValue }}</p>
       <div class="field label border no-margin no-padding" v-else>
         <input v-model="editingValue" @keyup.enter="toggleEdit" type="text" ref="inputRef" />
         <label>Prefix sentence</label>
       </div>
       <button
-        class="circle fill"
+        class="circle fill small"
         @click="toggleEdit"
         :aria-label="isEditing ? 'Save text' : 'Edit text'"
       >
         <i>{{ isEditing ? 'save' : 'edit' }}</i>
       </button>
     </nav>
+
     <p class="countdown-units bold primary">{{ formatTimeDifference(timeDifferenceMs) }}</p>
     <p
       class="countdown-units"
@@ -25,7 +27,10 @@
       after
     </p>
     <p class="countdown-units" v-else-if="!isSameDate">before</p>
-    <p class="countdown-units">the release of unrelated</p>
+
+    <p class="countdown-units">
+      the release of <span v-if="!prefixMentionsPumpUpTheJam">unrelated</span>
+    </p>
     <p class="countdown-units">Belgian Techno anthem</p>
     <p class="countdown-units bold">Pump Up The Jam</p>
     <p class="small-text">(Released as a single on 18 August 1989)</p>
@@ -188,12 +193,17 @@ const modelValue = defineModel({
   default: 'The chosen date is',
 })
 
+const prefixMentionsPumpUpTheJam = computed(() => {
+  return modelValue.value.toLowerCase().includes('pump up the jam')
+})
+
 const copyText = () => {
+  const unrelatedText = prefixMentionsPumpUpTheJam.value ? '' : 'unrelated '
   let textToCopy
   if (isSameDate.value) {
-    textToCopy = `${isToday(userDate.value) ? 'Today is' : modelValue.value} the same date as the release of unrelated Belgian Techno anthem "Pump Up The Jam" (Released as a single on 18 August 1989)`
+    textToCopy = `${isToday(userDate.value) ? 'Today is' : modelValue.value} the same date as the release of ${unrelatedText}Belgian Techno anthem "Pump Up The Jam" (Released as a single on 18 August 1989)`
   } else {
-    textToCopy = `${isToday(userDate.value) ? 'Today is' : modelValue.value} ${formatTimeDifference(timeDifferenceMs.value)} ${isToday(userDate.value) || (!isToday(userDate.value) && timeDifferenceMs.value > 0) ? 'after' : 'before'} the release of unrelated Belgian Techno anthem "Pump Up The Jam" (Released as a single on 18 August 1989)`
+    textToCopy = `${isToday(userDate.value) ? 'Today is' : modelValue.value} ${formatTimeDifference(timeDifferenceMs.value)} ${isToday(userDate.value) || (!isToday(userDate.value) && timeDifferenceMs.value > 0) ? 'after' : 'before'} the release of ${unrelatedText}Belgian Techno anthem "Pump Up The Jam" (Released as a single on 18 August 1989)`
   }
   navigator.clipboard.writeText(textToCopy)
 }
@@ -207,10 +217,19 @@ const copyText = () => {
   padding-inline: 16px;
 }
 
+.edit-prefix {
+  flex-direction: column-reverse;
+  gap: 0.5rem;
+
+  .countdown-units {
+    margin-block: 0;
+  }
+}
+
 .countdown-units {
   font-size: 1.5rem;
   margin-block: 0.25rem;
-  white-space: nowrap;
+  text-wrap: pretty;
 
   &.bold.primary {
     line-height: normal;

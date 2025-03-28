f57917930e6ed923b90f1ecc1a05a633e17f1aac
Julien Rutscho <58421711+Khoding@users.noreply.github.com>
2025-03-29 00:07:04 +0100

    today's date

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -18,7 +18,10 @@ const pumpUpTheJamReleaseDate = new Date(1989, 7, 18)
 const pumpUpTheJamYear = pumpUpTheJamReleaseDate.getFullYear()
 const pumpUpTheJamTimestamp = pumpUpTheJamReleaseDate.getTime()
 
-const selectedDate = ref(new Date().toISOString().split('T')[0])
+const today = new Date()
+const selectedDate = ref(
+  `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`,
+)
 
 const userDate = computed(() => {
   // Check if this is an extended date format
